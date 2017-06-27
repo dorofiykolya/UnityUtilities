cea7cd9a2878daae8101fa77a9f3f143ab5c29bd
dorofii <dorofiy@seriouscake.net>
2017-06-27 20:41:48 +0300

    implicit Lazy

--- Utils/Lazy.cs
+++ Utils/Lazy.cs
@@ -2,8 +2,20 @@
 
 namespace Utils
 {
-  public class Lazy<T>
+  public interface ILazy { }
+
+  public interface ILazy<out T> : ILazy
   {
+    T Value { get; }
+  }
+
+  public class Lazy<T> : ILazy<T>
+  {
+    public static implicit operator T(Lazy<T> lazy)
+    {
+      return lazy.Value;
+    }
+
     private readonly Func<T> _factory;
     private T _value;
     private bool _initialized;
