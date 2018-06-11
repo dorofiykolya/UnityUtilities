af4a4709f3b589a2c3b0c04708ad71bc21d442ba
dorofiy <dorofiykolya@gmail.com>
2018-06-11 17:27:18 +0300

    add transition

--- Utils/Tweens/TweenTransitions.cs
+++ Utils/Tweens/TweenTransitions.cs
@@ -15,5 +15,22 @@ namespace Utils.Tweens
       const double overshootOrAmplitude = 1.70158;
       return (float)(Math.Pow(invfloat, 2.0) * ((overshootOrAmplitude + 1.0) * invfloat + overshootOrAmplitude) + 1.0);
     }
+
+    public static float BounceOut(float ratio)
+    {
+      if (ratio < 1f / 2.75f)
+      {
+        return 7.5625f * ratio * ratio;
+      }
+      if (ratio < 2f / 2.75f)
+      {
+        return 7.5625f * (ratio -= 1.5f / 2.75f) * ratio + .75f;
+      }
+      if (ratio < 2.5f / 2.75f)
+      {
+        return 7.5625f * (ratio -= 2.25f / 2.75f) * ratio + .9375f;
+      }
+      return 7.5625f * (ratio -= 2.625f / 2.75f) * ratio + .984375f;
+    }
   }
 }
