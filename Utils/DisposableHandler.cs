a4c0a1f8dbf869ebed6f6936a6b48852a057bd30
dorofiy <dorofiykolya@gmail.com>
2018-09-13 16:57:27 +0300

    DisposableHandler

---
+++ Utils/DisposableHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Utils
+{
+  public class DisposableHandler : IDisposable
+  {
+    private Action _onDispose;
+
+    public DisposableHandler(Action onDispose)
+    {
+      _onDispose = onDispose;
+    }
+
+    public void Dispose()
+    {
+      if (_onDispose != null)
+      {
+        var dispose = _onDispose;
+        _onDispose = null;
+        dispose();
+      }
+    }
+  }
+}
