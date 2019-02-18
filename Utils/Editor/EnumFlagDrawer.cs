a34e6d223c27a2f9545b5e6bbce2d9d6c7dcc5df
dorofiy <dorofiykolya@gmail.com>
2019-02-18 19:26:51 +0200

    enum flags

---
+++ Utils/Editor/EnumFlagDrawer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace UnityEditor
+{
+  [CustomPropertyDrawer(typeof(EnumFlagAttribute))]
+  public class EnumFlagDrawer : PropertyDrawer
+  {
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+      EnumFlagAttribute flagSettings = (EnumFlagAttribute)attribute;
+      Enum targetEnum = (Enum)Enum.Parse(fieldInfo.FieldType, property.intValue.ToString());
+
+      string propName = flagSettings.name;
+      if (string.IsNullOrEmpty(propName))
+        propName = ObjectNames.NicifyVariableName(property.name);
+
+      EditorGUI.BeginProperty(position, label, property);
+      Enum enumNew = EditorGUI.EnumFlagsField(position, propName, targetEnum);
+      property.intValue = (int)Convert.ChangeType(enumNew, targetEnum.GetType());
+      EditorGUI.EndProperty();
+    }
+  }
+}
---
+++ Utils/EnumFlagAttribute.cs
@@ -0,0 +1,14 @@
+﻿namespace UnityEngine
+{
+  public class EnumFlagAttribute : PropertyAttribute
+  {
+    public string name;
+
+    public EnumFlagAttribute() { }
+
+    public EnumFlagAttribute(string name)
+    {
+      this.name = name;
+    }
+  }
+}
